d7dc99696c684ef022d7ce1401907b6b1ab5862a
(s) Brieanna Mulvey <bmulvey@pm7-0100.uopnet.plymouth.ac.uk>
2018-01-12 13:36:40 +0000

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,155 +1,140 @@
+//https://api.openaq.org/v1/cities
+
+//Use a text/string object to assign the city we would like to know the pollutant count.
+
 //Setup of variables
-let r = 0;
-let g = 0;
-let b = 0;
-let speed = 50;
-let diameter = 50;
-let x1;
-let y1;
-let x2;
-let y2;
-var value = false;
-var valueColor = false;
+let airQuality;
+let value = false;
+let randomCity = 0;
+let circleArray = [];
+let arraySize;
+let randR = 0;
+let randG = 0;
+let randB = 0;
 
-//Initialization function
-//Canvas size
-  function setup() {
-  var canvas = createCanvas(594, 841);
 
-//Name the following code to make it link to my index file.
-  canvas.parent("myContainer");
+//The preload function is executed before initializing the code in setup.
+//Loads the data I will be using.
+function preload() {
+//The URL is formatted according to the documentation provided by the developers in:
+//https://api.openaq.org/v1/cities
+  let url = "https://api.openaq.org/v1/cities";
+//The URL is sent to the loadJSON that returns the data to the city and air count variable.
+  airQuality = loadJSON(url);
+}
 
-//On setup ellipse appears in the middle of the screen.
-  x1 = width/2;
-  y1 = height/2;
-  x2 = width/2;
-  y2 = width/2;
-
-//Re maps the value of x1 and y1. New values will be constrained within 0 and half the width and height.
-  {let x1 = map(height, 0, width, 0, width/2);
-    let y1 = map(width, 0, height, height/2, 0);}
-
-//Origonal backround colour when the webpage is refreshed.
-    background(00);
+function setup() {
+//Return all JSON data in console.
+console.log(airQuality);
+  console.log(randomCity);
+  console.log(airQuality.results.length);
+for (i = 0; i < airQuality.results.length; i++){
+    console.log("City Name is " + airQuality.results[i].city );
   }
 
-//Press the enter key to change the values of r, g, b based on the valueColor variable.
-  function keyPressed() {
-    if (keyCode === ENTER && valueColor == false) {
-      valueColor = true;
+//Define canvas size, background colour, text position and text size.
+  createCanvas(1280, 720);
+  background(255);
+  textAlign(CENTER);
+  textSize(30);
+//Generating random colours for default state.
+  randR = random(255);
+  randG = random(255);
+  randB = random(255);
+
+//The following randomizes the city each time the page is loaded.
+//"Round" ensures the number is an integer so that a link can be made
+// to the data as the data contains no decimals.
+  randomCity = round(random(airQuality.results.length));
+//Sets array size of visual data to coincide with the pollutant count.
+  arraySize = airQuality.results[randomCity].count/1000;
+  for (let i=0; i<arraySize; i++){
+//The following line defines the initial starting position, speed of circles on x and y axis and size of circles.
+    circleArray[i] = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(10, 100));
+  }
+
+}
+function draw(){
+background(255);
+
+//Every time i is less than length of circle array this statement runs.
+  for (let i=0; i<circleArray.length; i++){
+    circleArray[i].moveFunction();
+    circleArray[i].displayCircle();
+  }
+//Display city and air pollutant count information on the screen.
+
+//This uses random numbers generating in setup to allocate a random colour to the text.
+fill (randR, randG, randB);
+text("Air Pollutant Count of " + airQuality.results[randomCity].city + " is " + airQuality.results[randomCity].count, width/2, 100 );
+}
+
+//When mouse is clicked the parameters such as fill and the data will change to another city at random.
+function mouseClicked(){
+  if (value == false){
+    value = true;
+    randomCity = round(random(airQuality.results.length));
+    background(255);
+
+    value = false;
+    randR = random(255);
+    randG = random(255);
+    randB = random(255);
+    console.log(randomCity);
+
+//Set size of the array as the number of pollutants divided by 1000.
+//When mouse is clicked the array set below will recur each time.
+    circleArray = [];
+    arraySize = airQuality.results[randomCity].count/1000;
+    for (let i=0; i<arraySize; i++){
+      circleArray[i] = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(10, 100));
     }
-    else if (keyCode === ENTER && valueColor == true) {
-      valueColor = false;
+    fill (randR, randG, randB);
+    text("Air Pollutant Count of " + airQuality.results[randomCity].city + " is " + airQuality.results[randomCity].count, width/2, 100 );
+
+  }
+}
+
+//Set up order of constructor within the class.
+class Circle{
+  constructor(x, y, speedX, speedY, size){
+//Setup of class' variables.
+    this.x = x;
+    this.y = y;
+    this.speedX = speedX;
+    this.speedY = speedY;
+    this.size = size;
+
+//Randomize the colours of the circles between different shades of grey.
+//Set "randomGrey" as a variable so that all colours have the same value when randomized.
+    let randomGrey = random(200);
+
+    this.rd = randomGrey;
+    this.grn = randomGrey;
+    this.bl = randomGrey;
+    this.a = 150;
+  }
+
+//Class function that controls motion and collision.
+  moveFunction(){
+//Motion system - current position and speed.
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY;
+
+//Based on boundaries collision, reverse direction for x and y.
+    if (this.x > width || this.x<0){
+      this.speedX *= -1;
+    }
+    if (this.y > (height) || this.y<0){
+      this.speedY *= -1;
     }
   }
 
-//Let computer know if the mouse is being clicked or not using value as boolean.
-//Console log used for debugging.
-  function mouseClicked(){
-    if (value == false){
-      value = true;
-      console.log("change to true");
-    }
-    else if (value == true) {
-      value = false;
-      console.log("change to false")
-    }
+//Class function that displays the ellipse
+  displayCircle(){
+    noStroke();
+    this.fillcol = color(this.rd, this.grn, this.bl, this.a)
+    fill(this.fillcol);
+    ellipse(this.x, this.y, this.size, this.size);
   }
-//Rendering function
-  function draw() {
-//Fill replaces the background colour over a period of time.
-//The first number denotes the colour of the background image which continuously recurs.
-//The bigger the second number, the quicker the the images are replaced.
-    fill(255, 1);
-//Size of canvas that is being replaced.
-    rect(0, 0, width, height);
-
-//Add to xy, y1, x2 and y2 a random movement between -5 to 5 for each frame due to the speed variable set at 5.
-//This makes the position of the shape move organically within those given parameters.
-//The constrain function will make sure that x1 and y1 will always
-//stay within the boundaries of the sketch canvas.
-    x1 += random(-speed, speed);
-    y1 += random(-speed, speed);
-    x1 = constrain(x1, 0, width-100);
-    y1 = constrain(y1, 0, height);
-    x2 = constrain(x2, 0, width-100);
-    y2 = constrain(y2, 0, height);
-
-//Mirrors the position of x1 and assigns value to x2.
-    x2=500-x1;
-    y2=500-y1;
-
-
-//To make value true, enter key must be pressed.
-//If value is true, then the colour of the shape will randomise
-//until value is false, which requires for the enter key to be pressed again.
-    if (valueColor == true) {
-      r = random(255);
-      g = random(255);
-      b = random(255);
-    }
-
-//Set up of second set of variables.
-//MouseY2 is the scale.
-    var mouseY2;
-//Middle2 and middle are used to centralize the shapes on the screen.
-    var middle2 = 50;
-    var middle = 150;
-
-
-//If the postion of the mouse on the Y axis is smaller than 50, then make the scale 1 (origonal).
-    if (mouseY < 50) {
-      mouseY2 = 1;
-    }
-//If this is not the case, then make the scale divided by 800 to create a smaller size.
-//The scale depends on the position of mouseY.
-    else  {
-      mouseY2 = mouseY/800;
-    }
-    scale(mouseY2);
-
-//If value is false, which is not mouse not clicked, then create ellipses.
-//This is automatically why the webpage opens on ellipses not rectangles.
-//Because the mouse has not yet been clicked to change to rectangles.
-//Translate used to handle difference of origin points for rect.
-    if (value == false) {
-
-      fill (r, g, b);
-      ellipse(x1+middle2, y1+middle, diameter, diameter);
-      ellipse(x2+middle2, y1+middle, diameter, diameter);
-      ellipse(x1+middle2, y2+middle, diameter, diameter);
-      ellipse(x2+middle2, y2+middle, diameter, diameter);
-
-//Duplicate origional set of circles, move 90 degrees and place.
-      push()
-      rotate(radians(90));
-      translate(0, -500);
-
-      ellipse(x1+middle, y1-middle2, diameter, diameter);
-      ellipse(x2+middle, y1-middle2, diameter, diameter);
-      ellipse(x1+middle, y2-middle2, diameter, diameter);
-      ellipse(x2+middle, y2-middle2, diameter, diameter);
-      pop()
-    }
-//If value is true, which is not mouse clicked, then create rectangles.
-//Translate used to handle difference of origin points for rect.
-    else if (value == true) {
-      fill(r, g, b);
-      translate(-25, -25);
-      rect(x1+middle2, y1+middle, diameter, diameter);
-      rect(x2+middle2, y1+middle, diameter, diameter);
-      rect(x1+middle2, y2+middle, diameter, diameter);
-      rect(x2+middle2, y2+middle, diameter, diameter);
-
-//Duplicate origional set of rectangles, move 90 degrees and place.
-      push();
-      rotate(radians(90));
-      translate(0, -550);
-
-      rect(x1+middle, y1-middle2, diameter, diameter);
-      rect(x2+middle, y1-middle2, diameter, diameter);
-      rect(x1+middle, y2-middle2, diameter, diameter);
-      rect(x2+middle, y2-middle2, diameter, diameter);
-      pop();
-    }
-  }
+}
